0878ba7b53676a4919375f91f1c16aad1a6e82bf
Jhaskel <johaskel@gmail.com>
2021-11-29 14:55:49 -0300

    af

--- src/main/java/com/doisbitsw/licencas/api/af/AfRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/af/AfRepository.java
@@ -20,10 +20,10 @@ public interface AfRepository extends JpaRepository<Af, Long> {
     List<Af> findByFornecedor(Long fornecedor);
 
     @Query(value = "SELECT af.*,SUM(ite.total) as tot,forn.alias as nomefor FROM af \n" +
-            "            INNER JOIN itens ite ON ite.af = af.code\n" +
-            "            INNER JOIN fornecedor forn ON forn.id = ite.fornecedor\n" +
-            "            WHERE ite.setor = :setor\n" +
-            "            GROUP BY af.code order by af.isenviado, af.id desc", nativeQuery = true)
+            "INNER JOIN itens ite ON ite.af = af.code\n" +
+            "INNER JOIN fornecedor forn ON forn.id = ite.fornecedor\n" +
+            "WHERE ite.setor = :setor\n" +
+            "GROUP BY af.code order by af.isenviado, af.id desc", nativeQuery = true)
     List<Af> findSetor(Long setor);
 
     //somente para testes
