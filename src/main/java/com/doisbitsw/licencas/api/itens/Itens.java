dce65a9b3cbfdf8e26f38643fbe41f4f1ef4b265
Jhaskel <johaskel@gmail.com>
2021-11-30 14:27:46 -0300

    pedidos

--- src/main/java/com/doisbitsw/licencas/api/itens/Itens.java
+++ src/main/java/com/doisbitsw/licencas/api/itens/Itens.java
@@ -16,38 +16,38 @@ public class Itens {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String cod;
-    private String pedido;
     private Long setor;
     private Long escola;
-    private Long nivel;
     private Long produto;
+    private String cod;
     private String alias;
-    private String nomeescola;
-    private String nomenivel;
+    private Long categoria;
+    private Long fornecedor;
+    private Long nivel;
     private String unidade;
     private Long ano;
+    private Long af;
+    private String pedido;
+    private String created;
+    private String nomeescola;
+    private String nomenivel;
+    private String processo;
     private Double quantidade;
     private Double valor;
     private Double total;
-    private Long af;
-    private String obs;
-    private String created;
-    private Long categoria;
-    private Long fornecedor;
     private Boolean ischeck;
     private Boolean isautorizado;
     private Boolean isagro;
     private String status;
     private String mes;
     private Boolean isativo;
-    private String processo;
+    private Long licitacao;
+    private String obs;
+
+    //nao no banco
     private Double tot;
     private String nomec;
-
-
     private Long local;
-
     private String modifiedAt;
 
 
