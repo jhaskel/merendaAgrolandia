d15a8ad72557ac03620ad30b83939a882f228413
Jhaskel <johaskel@gmail.com>
2021-12-01 21:51:06 -0300

    estoqueAdd

---
+++ src/main/java/com/doisbitsw/licencas/api/estoque/estoqueAdd/EstoqueAddDTO.java
@@ -0,0 +1,32 @@
+package com.doisbitsw.licencas.api.estoque.estoqueAdd;
+
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+
+@Data
+public class EstoqueAddDTO {
+    private Long id;
+    private Long produto;
+    private Long setor;
+    private String code;
+    private String alias;
+    private Double quantidade;
+    private String unidade;
+    private Long categoria;
+    private Long licitacao;
+    private Long fornecedor;
+    private Boolean agrofamiliar;
+    private Double valor;
+    private Long ano;
+    private String createdAt;
+    private Boolean isativo;
+    private String modifiedAt;
+    private String processo;
+
+
+
+    public static EstoqueAddDTO create(EstoqueAdd estoqueAdd) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(estoqueAdd, EstoqueAddDTO.class);
+    }
+}
