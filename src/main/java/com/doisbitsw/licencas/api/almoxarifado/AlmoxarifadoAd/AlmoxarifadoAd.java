1316e9f72e1dedef6f3e7428c8201c5af629bcb4
Jhaskel <johaskel@gmail.com>
2021-12-02 20:51:54 -0300

    almoxarifado

---
+++ src/main/java/com/doisbitsw/licencas/api/almoxarifado/AlmoxarifadoAd/AlmoxarifadoAd.java
@@ -0,0 +1,36 @@
+package com.doisbitsw.licencas.api.almoxarifado.AlmoxarifadoAd;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class AlmoxarifadoAd {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long escola;
+    private Long produto;
+    private Long licitacao;
+    private String alias;
+    private Long categoria;
+    private String unidade;
+    private String created;
+    private String nomeescola;
+    private Double quantidade;
+    private Boolean isativo;
+
+
+
+
+
+
+}
+
