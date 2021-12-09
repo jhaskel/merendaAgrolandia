ddddb98d03e32584fea767f1fba937e5eb27226e
Jhaskel <johaskel@gmail.com>
2021-12-09 09:47:46 -0300

    pnae

--- src/main/java/com/doisbitsw/licencas/api/pnae/Pnae.java
+++ src/main/java/com/doisbitsw/licencas/api/pnae/Pnae.java
@@ -23,7 +23,5 @@ public class Pnae {
     private Long cidade;
 
 
-
-
 }
 
--- src/main/java/com/doisbitsw/licencas/api/pnae/PnaeRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/pnae/PnaeRepository.java
@@ -1,10 +1,15 @@
 package com.doisbitsw.licencas.api.pnae;
 
+import com.doisbitsw.licencas.api.pedidos.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface PnaeRepository extends JpaRepository<Pnae, Long> {
 
+    @Query(value = "SELECT *, sum(valor) as soma  FROM pnae WHERE   ano = 2021", nativeQuery = true)
+    List<Pnae> findAll();
 
     @Query(value = "SELECT sum(valor) as soma  FROM pnae \n" +
             "WHERE    ano = :ano ", nativeQuery = true)
