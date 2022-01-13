ae40ed42e87d475ce0ae8a60560d74e8c2ce7c91
Jhaskel <johaskel@gmail.com>
2022-01-13 16:55:20 -0300

    pnae

--- src/main/java/com/doisbitsw/licencas/api/contabilidade/ContabilidadeRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/contabilidade/ContabilidadeRepository.java
@@ -10,7 +10,7 @@ public interface ContabilidadeRepository extends JpaRepository<Contabilidade, Lo
     @Query(value = "SELECT * FROM contabilidade where isativo = true ", nativeQuery = true)
     List<Contabilidade> findAll();
 
-    @Query(value = "SELECT * FROM contabilidade WHERE code = :code and nivel = :nivel and isativo = true ", nativeQuery = true)
+    @Query(value = "SELECT * FROM contabilidade WHERE id = :code and nivel = :nivel and isativo = true ", nativeQuery = true)
     List<Contabilidade> findCod(Long code,Long nivel);
 
     @Query(value = "SELECT * FROM contabilidade WHERE nivel = :nivel and isativo = true  ", nativeQuery = true)
